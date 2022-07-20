16ce1ee46a847a1f994637695d0810fb7ecbaf14
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-07-20 01:46:46 +0100

    Add name and score to the list items

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 
 import './style.css';
+
+
+
+const ul = document.querySelector('.names-container')
+const name = document.querySelector('.input-name')
+const score = document.querySelector('.input-score')
+const submitBtn = document.querySelector('.submit-btn')
+
+
+const addScore = () => {
+    if(name.value === '' && score.value === '') return;
+    const li = `<li>${name.value}:${score.value}</li>`
+    ul.insertAdjacentHTML('beforeend', li)
+    name.value = ''
+    score.value = ''
+    console.log(li)
+}
+
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    if(isNaN(name.value)) {
+        addScore()
+    } else {
+        alert('The name field should contain letters')
+    }
+    console.log('i have been clicked')
+})
