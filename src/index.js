c15b7b842e76b135642e460285ba361c6d24b8bb
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-07-20 01:55:55 +0100

    Componentized my functions and logic

--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
-
 import './style.css';
+import addScore from './scores';
 
+const name = document.querySelector('.input-name');
+const submitBtn = document.querySelector('.submit-btn');
 
-
-const ul = document.querySelector('.names-container')
-const name = document.querySelector('.input-name')
-const score = document.querySelector('.input-score')
-const submitBtn = document.querySelector('.submit-btn')
-
-
-const addScore = () => {
-    if(name.value === '' && score.value === '') return;
-    const li = `<li>${name.value}:${score.value}</li>`
-    ul.insertAdjacentHTML('beforeend', li)
-    name.value = ''
-    score.value = ''
-    console.log(li)
-}
-
+addScore();
 
 submitBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    if(isNaN(name.value)) {
-        addScore()
-    } else {
-        alert('The name field should contain letters')
-    }
-    console.log('i have been clicked')
-})
+  e.preventDefault();
+  if (isNaN(name.value)) {
+    addScore();
+  } else {
+    alert('The name field should contain letters');
+  }
+  console.log('i have been clicked');
+});
